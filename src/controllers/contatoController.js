964de1c16e674f4af353b32efcf33fe12fd7856b
edumpda <edumarques.pereira@gmail.com>
2023-08-20 17:34:37 -0300

    Initial

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,70 @@
+const Contato = require('../models/ContatoModel');
+
+exports.index = (req, res) => {
+    res.render('contato', {
+        contato: {}
+    });
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body);
+        await contato.register();
+        
+        if(contato.errors.lenght > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+    
+        req.flash('success', 'Contato registrado com sucesso.');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.editIndex = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const user = await Contato.buscaPorId(req.params.id);
+    if (!user) return res.render('404');
+
+    res.render('contato', {
+        contato: user
+    });
+};
+
+exports.edit = async function(req, res) {
+    try {
+        if(!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+    
+        if(contato.errors.lenght > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+    
+        req.flash('success', 'Contato editado com sucesso.');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+};
+
+exports.delete = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const user = await Contato.delete(req.params.id);
+    if (!user) return res.render('404');
+
+    req.flash('success', 'Contato deletado com sucesso.');
+    req.session.save(() => res.redirect('back'));
+    return;
+}
